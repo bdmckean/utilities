978516a17d2c544173d93cb74d77d5bbe2e69b74
Brian <bdmckean@gmail.com>
2018-02-22 12:35:16 +0100

    added bootstrap_dev.sh

---
+++ general/bootstrap_dev.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Update
+
+
+# Install Anaconda
+
+
+# Install jupyter notebook
+
+
+# Install mongodb
+
+
+# Install Python packages
+conda install pymongo
+
+
+
+# Get setup for LaTeX
+conda install nbconvert
+sudo apt-get install pandoc
+sudo apt-get install -y texlive-xetex
